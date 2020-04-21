d61b2b021c20586a183798f4d81e8c22ae191e67
eozuna3 <eo30239@gmail.com>
2020-04-20 20:37:05 -0500

    Linked the survery button to survery.html page, created a dummy friends object in the friends.js file and started to add rounting code to htmlRoutes.js.

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+  {
+    name: "Mike Smith",
+    photo: "https://images.unsplash.com/photo-1505640070685-2a70292000ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+    ]
+  }
+]
+
+module.exports = friendsArray;
