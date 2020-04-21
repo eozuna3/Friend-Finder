5591541109a6a1e03528f52ce1f73617720d0f7a
eozuna3 <eo30239@gmail.com>
2020-04-20 19:50:19 -0500

    created working server using express in server.js file.

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 7500;
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
