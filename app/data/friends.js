862b404c1eac6d0207a49909d078a0c9903264e7
eozuna3 <eo30239@gmail.com>
2020-04-22 22:50:31 -0500

    Created multiple friend objects for testing in friends file.  Created a function in apiroutes to calculate the position of the friends object with the small difference from the newUser object.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@ var friendsArray = [
     photo: "https://images.unsplash.com/photo-1505640070685-2a70292000ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
     scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   },
-  /*{
+  {
     name: "Jenny Shabazz",
     photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -13,7 +13,7 @@ var friendsArray = [
     name: "Isabella Marquez",
     photo: "https://images.pexels.com/photos/1988681/pexels-photo-1988681.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     scores: [3, 4, 1, 2, 5, 1, 3, 2, 1, 5]
-  }*/
+  }
 ]
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,17 +12,25 @@ module.exports = function (app) {
     console.log("Here are the scores for the newUser: " + newUser.scores);
     console.log("here is the current length of the friendsArray: " + friendsData.length);
     for (let index = 0; index < friendsData.length; index++) {
-       var individualScores = friendsData[index].scores;
-       console.log("Here are the scores for each individual in the friendsArray: " + individualScores);
-       var totalDifference = 0;
-       for(var x = 0; x < 10; x++){
-         totalDifference += (Math.abs(individualScores[x] - newUser.scores[x]));
-       }
-       console.log(totalDifference);
+      var individualScores = friendsData[index].scores;
+      console.log("Here are the scores for each individual in the friendsArray: " + individualScores);
+      var totalDifference = 0;
+      for (var x = 0; x < 10; x++) {
+        totalDifference += (Math.abs(individualScores[x] - newUser.scores[x]));
+      }
+      console.log(totalDifference);
       differencesArray.push(totalDifference);
     }
     console.log(differencesArray);
     friendsData.push(newUser);
-    return res.json(friendsData);
+    return res.json(friendsData[arrayMin(differencesArray)]);
   })
+}
+
+function arrayMin(arr) {
+  var lowest = 0;
+ for (var i = 1; i < arr.length; i++) {
+  if (arr[i] < arr[lowest]) lowest = i;
+ }
+ return lowest;
 }
